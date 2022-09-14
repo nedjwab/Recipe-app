aee4afa717bd210753244910ae3d333fafa01818
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-14 21:42:50 +0100

    Fix User sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
   def create; end
   def show; end
+
+  def set_user 
+    @user = User.find(params[:id])
+  end
 end
